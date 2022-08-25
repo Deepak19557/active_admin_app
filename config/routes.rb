32a7fb4dfa90b96c8f035e58ebaaaea99e99c496
deepak chourasiya <chourasiya123deepak@gmail.com>
2022-08-25 15:34:45 +0530

    user added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/new'
   ActiveAdmin.routes(self)
+  resources :users
+  root 'users#index'
+  patch '/set_name', to: "posts#set_name"
+
 
    resource :locks, :controller => 'locks' do
       collection do
